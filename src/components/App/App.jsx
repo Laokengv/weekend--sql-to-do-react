2252911e115ce7e77518eab78d03bd20ebcb0c39
Laokeng Vang <laokengv@gmail.com>
2023-06-07 00:48:05 -0500

    an h2 with label tag, added input for text and button to add tasks.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,24 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
-function App () {
-  
+function App() {
+
   return (
-    <div>
+    <div className="to-do-list">
       <h1>TO DO APP</h1>
+      <form>
+        <h2 className="label-tag">
+          <label htmlFor="new-todo-input" className="label_one">
+            Task of the Day
+          </label>
+        </h2>
+        <input type="text" id="new-todo-input" className="input-bar" name="text" />
+        <button type="submit" className="add-task-btn">
+          Add
+        </button>
+      </form>
+      <div className="">
+        <button type="button" className=""></button>
+      </div>
     </div>
   );
 
