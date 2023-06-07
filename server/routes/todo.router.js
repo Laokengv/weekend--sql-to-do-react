5a13798664e0cd8b8d67afff79165a6856e489f6
Laokeng Vang <laokengv@gmail.com>
2023-06-07 17:58:06 -0500

    Restarted and cleared my App.jsx. Added get function and post function

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,9 +3,34 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
+router.get('/', (req, res) => {
+    console.log("In GET request");
+    let query = 'SELECT * FROM "toDoApp";';
 
+    pool.query(query).then((result) => {
+        res.send(result.rows);
+    }) .catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    })
+});
 // POST
-
+router.post('/', (req, res) => {
+    const {task, taskCompleted} = req.body;
+    console.log(req.body);
+    const query = `
+    INSERT INTO "toDoApp" ("task", "taskCompleted")
+    VALUES ($1, $2);
+    `;
+    pool.query(query, [task, taskCompleted])
+    .then((result) => {
+        res.sendStatus(201);
+    })
+    .catch((error) => {
+        console.log(`Error ${query}`, error);
+        res.sendStatus(500);
+    })
+})
 // PUT
 
 // DELETE
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,35 +1,43 @@
+
 import { useState } from 'react';
+
+
+{/* start GET request */}
+function getToDo() {
+  return fetch('/toDoApp')
+  .then((response) => response.json())
+  .catch((error) => {
+    console.log(error);
+  });
+} {/* end GET request */}
+
+{/* start POST request */}
+function addTask(toDoApp) {
+  return fetch('/toDoApp', {
+    method: 'POST',
+    body: JSON.stringify(toDoApp),
+    headers: { "Content-Type": "application/json" }
+  })
+  .then((response) => {
+    if(response.status !== 201) {
+      throw new Error('Bad Status!');
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+} {/* end POST request */}
+
 
 function App() {
 
   return (
-    <div className="to-do-list">
-      <h1>TO DO APP</h1>
-      <form>
-        <h2 className="label-tag">
-          <label htmlFor="new-todo-input" className="label_one">
-            Task of the Day
-          </label>
-        </h2>
-        <input type="text" id="new-todo-input" className="input-bar" name="text" />
-        <button type="submit" className="add-task-btn">
-          Add
-        </button>
-      </form>
-      <div className="">
-        <button type="button" className=""></button>
-      </div>
-      <h2>{} tasks remaining</h2>
-      <ul role="list" className="todo-list heading" aria-labelledby="heading-list">
-        <li className="">
-          <div className="cb">
-            <input id="todo-0" type="checkbox" defaultChecked={true} />
-            <label className="todo-label" htmlFor="todo-0">
-              Eat 
-            </label>
-          </div>
-        </li>
-      </ul>
+    <div className="todo">
+      <main>
+        <form onSubmit={todoSubmit}>
+          <input type="text" placeholder="task" value={} onChange={}/>
+        </form>
+      </main>
     </div>
   );
 
